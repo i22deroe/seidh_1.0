8a219a8b0b4b4379a9cab7685f2f67767fd2e70a
Enrique Delgado Rodríguez <i22deroe@uco.es>
2018-06-16 16:14:46 +0200

    Create props.py
    Props.py is the old constants.py. Updated for consistency.

---
+++ lib/props/props.py
@@ -0,0 +1,16 @@
+'''
+	This file contains the constants and directories used by Seidh.
+	It also especifies some basic configuration such as:
+	->MAX_AA: number of tries per aminoacid in a single peptide simulation
+	->MAX_PEPTIDES: number of peptide simulations
+	->NUM_PROC: number of processors to use. 
+
+'''
+
+predictions="pdb_files/predictions/"
+peptides="pdb_files/peptides/"	
+proteins="pdb_files/proteins/"
+entities="pdb_files/entities/"
+MAX_AA=30
+MAX_PEPTIDES=50
+MIN_CONTACT=0.80
